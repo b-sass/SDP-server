f36a68cd226d59827da162791d094dc71a256209
Bartosz Sass <sassbartek@gmail.com>
2025-03-12 00:12:01 +0000

    eGFR calculations adult + child.

--- src/routes/calculate.js
+++ src/routes/calculate.js
@@ -3,23 +3,39 @@ const router = Router();
 
 router.put("/calculate", (req, res) => {
     const calculations = req.body;
-    let results = calculateEGFR(calculations);
+    let results = [];
+
+    calculations.forEach(c => {
+        if (c.height == null) {
+            results.push(
+                { eGFR: calculateEGFR(c) }
+            );
+        } else {
+            results.push(
+                { eGFR: calculateChildEGFR(c) }
+            );
+        }
+    });
     res.json(results).status(200);
 });
 
 let calculateEGFR = (patientStats) => {
-    let results = [];
-    patientStats.forEach(stats => {
-        let sex = 1; let race = 1;
-        if(stats.sex === "female") { sex = 0.742 };
-        if(stats.race === "black") { race = 1.210 };
-        let eGFR = 186 * Math.pow((stats.creat / 88.4), -1.154) * Math.pow(stats.age, -0.203) * sex * race;
-        results.push({
-            NSHID: stats.id,
-            eGFR: eGFR,
-        });
-    });
-    return results;
+    let sex = 1; let race = 1;
+    if(patientStats.sex === "female") { sex = 0.742 };
+    if(patientStats.race === "black") { race = 1.210 };
+    
+    console.log("Adult eGFR calculation");
+    return 186 * Math.pow((patientStats.creat / 88.4), -1.154) * Math.pow(patientStats.age, -0.203) * sex * race;
+}
+
+let calculateChildEGFR = (patientStats) => {
+    let k = 0.55;
+    if (patientStats.age < 1) k = 0.45;
+    if (patientStats.age >= 13 
+        && patientStats.sex == "male") k = 0.7;
+    
+    console.log("Child eGFR calculation");
+    return (patientStats.height * k) / (patientStats.creat / 88.4);
 }
 
 export default router;
