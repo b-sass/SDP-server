8b2c8c41f2d5dfb202418c7a08a1ac29f4ee422b
Bartosz Sass <sassbartek@gmail.com>
2025-04-08 01:17:53 +0100

    missing import

--- src/controller/mfa.js
+++ src/controller/mfa.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import * as mfa from "../middleware/mfa.js";
 import Clinician from "../models/Clinician.js";
 import Patient from "../models/Patient.js";
